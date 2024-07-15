a84dedb65814a72f9ac7627620e883f3f803bdd8
Ogechi Azurunwa <oazurunwa@byteworks.com.ng>
2024-07-15 16:18:16 +0100

    02 The Basics - String Interpolation

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styles: [`
+  h4 {
+    padding: 0.5rem;
+    background-color: palegreen;
+    border: 0.2rem solid green;
+  }
+  `]
+})
+export class SuccessAlertComponent {
+
+}
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styles: [`
+  h4 {
+    padding: 0.5rem;
+    background-color: mistyrose;
+    border: 0.2rem solid red;
+  }
+  `]
+})
+export class WarningAlertComponent {
+
+}
