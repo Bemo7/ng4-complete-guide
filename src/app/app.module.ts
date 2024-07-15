65de5aea3e7ba78dd08c4b874dfbada143628f63
Ogechi Azurunwa <oazurunwa@byteworks.com.ng>
2024-07-15 13:10:55 +0100

    02 The Basics - Fully Understanding the Component Selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+// import { HttpModule } from "@angular/http";
+import {FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
