58c932e0f8359b8897e708759b2984cfd0a9a27d
Bemo7 <benjaminedet7@gmail.com>
2024-07-15 23:10:00 +0100

    02 The Basics - Two-Way-Databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,24 +5,24 @@ import { Component } from '@angular/core';
   // selector: '.app-servers',
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrl: './servers.component.css'
+  styleUrl: './servers.component.css',
 })
 export class ServersComponent {
-  allowNewServer: boolean = false
-  serverCreationStatus: string  ="No server was created"
-  serverName = ''
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created';
+  serverName = 'TestServer';
 
-  constructor(){
-    setTimeout(() =>{
-      this.allowNewServer= true
-    },2000)
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
-  onCreateServer(){
-    this.serverCreationStatus = "Server was created"
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
   }
 
-  onUpdateServerName(event: Event){
-    this.serverName = (<HTMLInputElement>event.target).value
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
