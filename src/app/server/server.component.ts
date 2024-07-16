94c36c49b4f17fc3b724702e16eb8a440f616886
Ogechi Azurunwa <oazurunwa@byteworks.com.ng>
2024-07-16 10:26:22 +0100

    02 The Basics - Styling Elements Dynamically with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,14 +1,22 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html'
+  selector: 'app-server',
+  templateUrl: './server.component.html',
 })
 export class ServerComponent {
-serverId:number = 10;
-serverStatus:string = "active"
+  serverId: number = 10;
+  serverStatus: string = 'offline';
 
-getServerStatus():string {
-    return this.serverStatus
-}
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getServerStatus(): string {
+    return this.serverStatus;
+  }
+
+  getColor(): string {
+    return this.serverStatus === 'online' ? 'palegreen' : 'mistyrose';
+  }
 }
